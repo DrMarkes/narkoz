ca657d61217fcc0f9a77927386ef2b722235d601
DrMarkes <markovandrew@yandex.ru>
2017-03-30 22:30:19 +0700

    подключена barryvdh/laravel-ide-helper

--- app/UserQuestion.php
+++ app/UserQuestion.php
@@ -4,6 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\UserQuestion
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $content
+ * @property string $status
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property-read \App\User $user
+ * @method static \Illuminate\Database\Query\Builder|\App\UserQuestion whereContent($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\UserQuestion whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\UserQuestion whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\UserQuestion whereStatus($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\UserQuestion whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\UserQuestion whereUserId($value)
+ * @mixin \Eloquent
+ */
 class UserQuestion extends Model
 {
     protected $table = "user_questions";
